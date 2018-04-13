33fde45e44a3fc9c8ced1e6f1ecbd4a8e3432495
jma26 <j.masonoma17@gmail.com>
2018-04-13 16:27:54 -0700

    Updated with animated scrolling in index.js and a footer section

--- js/index.js
+++ js/index.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
     console.log('HELLO');
 
-    $(document).on('click', 'a[href^="#"]', function(event) {
-        window.setTimeout(function() {
-            offsetAnchor();
-        }, 0);
+    // Prevent URL hash & Animate scrolling
+    $('.nav-link').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+        scrollTop: $(this.hash).offset().top - 50}, 1100);
     });
-
-    window.setTimeout(offsetAnchor, 0);
-
-
-    function offsetAnchor() {
-        if (location.hash.length !== 0) {
-            window.scrollTo(window.scrollX, window.scrollY - 60);
-        }
-    }
+    $('.navbar-brand').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 1100);
+    });
 });
