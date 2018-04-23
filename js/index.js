f536b1d545f5b34724acdb0569ab257a8fb23b09
jma26 <j.masonoma17@gmail.com>
2018-04-23 16:09:06 -0700

    Update project section with bootstrap 4 modals and project placeholder

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     console.log('HELLO');
+    // Default hide pop up info
+    $('.popupContainer').hide();
 
     // Prevent URL hash & Animate scrolling
     $('.navScroll').click(function(event) {
@@ -7,8 +9,8 @@ $(document).ready(function() {
         $('html, body').animate({
         scrollTop: $(this.hash).offset().top - 50}, 1100);
     });
-    $('.navbar-brand').click(function(event) {
+    $(".navbar-brand").click(function(event) {
         event.preventDefault();
-        $('html, body').animate({scrollTop: 0}, 1100);
+        $("html, body").animate({scrollTop: 0}, 1100);
     });
 });
