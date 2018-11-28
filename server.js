7480933c20b51cba9a50adfb7c567a2e67fb9207
Jesse Ma <j.masonoma17@gmail.com>
2018-11-27 18:08:51 -0800

    Package.json: Add dev dependencies and scripts

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express =  require('express');
 const path = require('path');
+
+// Create express server instance
 const app = express();
 
 const port = 8000;
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
