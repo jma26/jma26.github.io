6164ca11ab7173060e28e7b3fefb128bf76d2fe9
Jesse Ma <j.masonoma17@gmail.com>
2018-12-07 15:12:29 -0800

    Update index.js with animated scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,25 @@
 $(document).ready(function() {
     console.log('Testing index.js....');
     $('.nav-items').click(function(event) {
-        $('html, body').animate({
-            scrollTop: $(this.hash).offset().top
-        }, 1000)
+        event.preventDefault();
+        console.log($(this.hash).position());
+        var target = $(this).attr('href');
+        if (target === '#about') {
+            $('.main').animate({
+                scrollTop: 0
+            }, 1200);
+        } else if (target === '#skills') {
+            $('.main').animate({
+                scrollTop: 891
+            }, 1200);
+        } else if (target === '#projects') {
+            $('.main').animate({
+                scrollTop: 1782
+            }, 1200)
+        } else if (target === '#contact') {
+            $('.main').animate({
+                scrollTop: 2737
+            }, 1200)
+        }
     })
 })
