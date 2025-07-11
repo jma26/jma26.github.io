244c4ba723567fd20b7e2896d91f5a4441ff04e4
Jesse Ma <jma26code@gmail.com>
2025-07-11 00:22:23 -0700

    Remove navbar component (no longer needed), add footer component section

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Footer, FooterCopyright, FooterLinkGroup, FooterLink } from "flowbite-svelte";
+  import { EnvelopeSolid, GithubSolid, LinkedinSolid } from 'flowbite-svelte-icons';
+</script>
+
+<Footer class="container my-8 mx-auto p-0 shadow-none md:p-0 lg:my-0">
+  <!-- <FooterLinkGroup class="flex text-sm text-gray-500 sm:mt-0 dark:text-gray-400">
+    <FooterLink href="/">
+      <EnvelopeSolid />
+    </FooterLink>
+    <FooterLink href="/">
+      <GithubSolid />
+    </FooterLink>
+    <FooterLink href="/">
+      <LinkedinSolid />
+    </FooterLink>
+  </FooterLinkGroup> -->
+  <FooterCopyright href="/" by="Jesse Ma" copyrightMessage="— All Rights Reserved." year={new Date().getFullYear()} />
+</Footer>
