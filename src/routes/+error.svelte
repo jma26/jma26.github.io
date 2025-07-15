ba4511b616a4df2a98b461d8cf93b9af5878286d
Jesse Ma <jma26code@gmail.com>
2025-07-15 11:09:44 -0700

    Add error component

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { Button } from "flowbite-svelte";
+	import { page } from '$app/state';
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen px-6 text-center w-full md:px-12 lg:py-24">
+  <h1 class="text-6xl font-extrabold text-white tracking-tight leading-none md:text-9xl">
+    {#if page.status === 404}
+      4 😮 4
+    {:else}
+      {page.status}
+    {/if}
+  </h1>
+  <p class="my-8 text-lg text-white tracking-tight">The page you were looking for was moved or doesn't exist.</p>
+  <Button class="bg-orange-600 border-orange-600 mx-auto place-self-start duration-300 focus-within:ring-0 hover:bg-orange-500" href="/" size="sm">Go Back</Button>
+</div>
