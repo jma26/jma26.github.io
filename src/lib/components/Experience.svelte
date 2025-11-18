9c92c2ab8f4efa419d1aa2680cdd2997ef768ca4
Jesse Ma <jma26code@gmail.com>
2025-11-18 14:14:04 -0800

    Update experience section into bullets and resume link...again

--- src/lib/components/Experience.svelte
+++ src/lib/components/Experience.svelte
@@ -12,26 +12,38 @@
           <TimelineItem h3Class="mb-0 text-white" liClass="ms-8 relative" title="Web Developer · Lifetech Resources" date="2021 - June 2025">
             {#snippet orientationSlot()}
               <span class="bg-orange-600 absolute -start-11 h-6 w-6 rounded-full ring-6 ring-white top-1"></span>
-            {/snippet}  
+            {/snippet}
+            <ul class="list-disc ml-4 my-4 text-white">
+              <li>Collaborated with UI/UX and marketing teams in implementing a new design system across multiple Shopify storefronts, improving design consistency and user engagement metrics</li> 
+              <li>Built and maintained MERN stack web applications critical for distributor access to marketing assets</li>
+              <li>Supported marketing campaigns by building out mobile-first, responsive promotional landing pages and emails</li>
+              <li>Architected and executed the migration of the company website from PHP Laravel to WordPress, implementing custom theme development, plugin integration, and complete data migration, resulting in a more maintainable and manageable platform for non-technical staff</li>
+              <li>Identified areas of improvements in site performance with analytic tools (Lighthouse/Clarity), reducing average page load times and increasing conversion rates</li>
+            </ul>
             <p class="my-4 text-white">
-              Collaborated with UI/UX and marketing teams in implementing a new design system across multiple Shopify storefronts. Supported marketing campaigns by building out promotional landing pages, emails. Built and maintained RESTful APIs connecting Shopify to Mailchimp and other platforms, enabling automated marketing workflows and real-time customer data synchronization. Implemented new features and functionalities on corporate distributor asset portal using MERN stack.
             </p>
             <div class="flex flex-wrap gap-2">
               <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">HTML</Badge>
               <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">SCSS/CSS</Badge>
               <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">JavaScript</Badge>
+              <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">MongoDB</Badge>
+              <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">Express.js</Badge>
               <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">React</Badge>
               <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">Node.js</Badge>
               <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">Next.js</Badge>
-              <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">MongoDB</Badge>
               <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">Shopify Liquid</Badge>
             </div>
           </TimelineItem>
           <TimelineItem h3Class="mb-0 text-white" liClass="ms-8 relative" title="Web Developer · Spec-D Tuning" date="2019 - 2021">
             {#snippet orientationSlot()}
               <span class="bg-orange-600 absolute -start-11 h-6 w-6 rounded-full ring-6 ring-white top-1"></span>
-            {/snippet}  
-            <p class="my-4 text-white">Directed the end-to-end migration of legacy eCommerce storefronts to BigCommerce, including custom Stencil theme development, data migration, and integration of third-party marketing and analytic tools. Collaborated with marketing teams to launch targeted landing pages and PPC campaigns. Managed and updated product listings, including adding new products, optimizing descriptions, images, and metadata to ensure accurate data and enhanced product discoverability</p>
+            {/snippet}
+            <ul class="list-disc ml-4 my-4 text-white">
+              <li>Directed the end-to-end migration of legacy eCommerce storefronts to BigCommerce, including custom Stencil theme development, data migration, and integration of third-party marketing and analytic tools, resulting in an increase in conversion rates</li> 
+              <li>Leveraged analytic tools to identify and resolve site performance issues</li>
+              <li>Collaborated with marketing teams to launch targeted landing pages and PPC campaigns, boosting ad ROI and user engagement</li>
+              <li>Managed and updated product listings, including adding new products, optimizing descriptions, images, and metadata to ensure accurate data and enhanced product discoverability</li>
+            </ul>
             <div class="flex flex-wrap gap-2">
               <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">HTML</Badge>
               <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">SCSS/CSS</Badge>
@@ -43,7 +55,9 @@
             {#snippet orientationSlot()}
               <span class="bg-orange-600 absolute -start-11 h-6 w-6 rounded-full ring-6 ring-white top-1"></span>
             {/snippet}  
-            <p class="my-4 text-white">Collaborated with cross-functional teams of designers and developers to implement core platform features in alignment with business initiatives</p>
+            <ul class="list-disc ml-4 my-4 text-white">
+              <li>Collaborated with cross-functional teams of designers and developers to implement core platform features in alignment with business initiatives</li> 
+            </ul>
             <div class="flex flex-wrap gap-2">
               <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">HTML</Badge>
               <Badge border class="bg-orange-600 border-orange-600 py-1 px-2 text-white">SCSS/CSS</Badge>
@@ -53,6 +67,6 @@
             </div>
           </TimelineItem>
         </Timeline>
-        <A class="flex gap-2 mt-12 place-self-start text-white duration-300 active:text-orange-600 focus:text-orange-600 hover:text-orange-500" href="https://drive.google.com/file/d/1eeNTRijnOrymLchA4ZqPeuU4hGql_JGh/view?usp=sharing" target="_blank">View Resume <ArrowUpRightFromSquareOutline /></A>
+        <A class="flex gap-2 mt-12 place-self-start text-white duration-300 active:text-orange-600 focus:text-orange-600 hover:text-orange-500" href="https://drive.google.com/file/d/17Ke6um6-cdgnQztISq7P8lGON_txNtID/view?usp=sharing" target="_blank">View Resume <ArrowUpRightFromSquareOutline /></A>
       </article>
     </section>
